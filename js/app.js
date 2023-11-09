e37e0f62e34cdc9b3812dd03c0be4405167bb462
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-11-09 14:49:43 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
 
 $(".konsistensi .imgs").attr({ "data-aos": "zoom-in", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
+$(".konsistensi .logo").attr({ "data-aos": "fade-right", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
+
 $(".scroll").click(function () {
 
     $('html, body').animate({
