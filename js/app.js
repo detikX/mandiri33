7256f74a3336cbbef001ff70227b4d64272be78e
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-11-10 10:51:45 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@ $(document).ready(function () {
     AOS.init();
 })
 
-$(".konsistensi .imgs").attr({ "data-aos": "zoom-in", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
+$(".konsistensi .imgs, .kualitas .gambars, .dpk .data-dpk").attr({ "data-aos": "zoom-in", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
+
+$(".dpk .img, .kredit .img").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
+
+$(".kualitas .orangs").attr({ "data-aos": "fade-left", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
 $(".konsistensi .logo").attr({ "data-aos": "fade-right", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
